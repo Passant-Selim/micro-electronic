0058f9057d9ad30f745b7bdb361728fb4f3a4738
Passant Selim <passantselim22@gmail.com>
2026-02-14 14:07:50 +0200

    commit

--- db/database.js
+++ db/database.js
@@ -4,7 +4,7 @@ const mongo_url = process.env.MONGO_URL;
 
 async function dbConnection () {
     try {
-        mongoose.connect(mongo_url);
+       await mongoose.connect(mongo_url);
         console.log("db is connected");
     } catch (error) {
         console.log(error);
